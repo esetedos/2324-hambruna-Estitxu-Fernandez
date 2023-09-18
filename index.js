95ef3eba6ab4285f4def86936b74ffaf30ddc927
esetedos <esther.fernandez@ikasle.aeg.eus>
2023-09-18 18:04:58 +0200

    [FIX] primer ejercicio completado (faltaba que si el resultado es más de un tipo de donut, que salieran todos y no solo uno)

--- index.js
+++ index.js
@@ -7,34 +7,76 @@ const fetchAsyncData = async () => {
     try {
         const result = (await getAllTodos())["items"].item
 
+
+        console.log("1. Primer ejercicio");
         // donut con más azúcar
+        // const sugar = nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars;
         let maxSugar = result.map(element => {
             element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars = parseInt(element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars.slice(0, element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars.length-1));
         })
         maxSugar  = result.sort((x, y) => y.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars - x.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars);
-        console.log("Donut con más azúcar: " + maxSugar[0].name);
-      
+        maxSugar = maxSugar.filter(element => element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars === maxSugar[0].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars)
+        
+        console.log("Donut con más azúcar: ");
+        
+        maxSugar.map(element => {
+            console.log(element.name);
+            console.log();
+        })
+        console.log();
+
+
         // donut con más hierro
         let maxIron = result.map(element => {
             element.nutrition_facts.nutrition.vitamines[3].percent = parseInt(element.nutrition_facts.nutrition.vitamines[3].percent.slice(0, element.nutrition_facts.nutrition.vitamines[3].percent.length-1));
         })
         maxIron  = result.sort((x, y) => y.nutrition_facts.nutrition.vitamines[3].percent - x.nutrition_facts.nutrition.vitamines[3].percent);
-        console.log("Donut con más hierro: " + maxIron[0].name);
+        maxIron = maxIron.filter(element => element.nutrition_facts.nutrition.vitamines[3].percent === maxSugar[0].nutrition_facts.nutrition.vitamines[3].percent)
+        
+        console.log("Donut con más hierro: ");
+        
+        maxIron.map(element => {
+            console.log(element.name);
+            console.log();
+        })
+        console.log();
+
 
         // donut con más proteína
         let maxProteine = result.map(element => {
             element.nutrition_facts.nutrition.proteine = parseInt(element.nutrition_facts.nutrition.proteine.slice(0, element.nutrition_facts.nutrition.proteine.length-1));
         })
-        maxProteine  = result.sort((x, y) => y.nutrition_facts.nutrition.proteine- x.nutrition_facts.nutrition.proteine);
-        console.log("Donut con más proteína: " + maxProteine[0].name);
+        maxProteine  = result.sort((x, y) => y.nutrition_facts.nutrition.proteine - x.nutrition_facts.nutrition.proteine);
 
-        // donut con más fibra
+        maxProteine = maxProteine.filter(element => element.nutrition_facts.nutrition.proteine === maxSugar[0].nutrition_facts.nutrition.proteine)
+        
+        console.log("Donut con más proteína:");
+        
+        maxProteine.map(element => {
+            console.log(element.name);
+            console.log();
+        })
+
+        console.log();
+
+        // donut con menos fibra
         let maxFibre = result.map(element => {
             element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre = parseInt(element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre.slice(0, element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre.length-1));
         })
         maxFibre  = result.sort((x, y) => y.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre- x.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre);
-        console.log("Donut con más proteína: " + maxFibre[0].name);
 
+        maxFibre = maxFibre.filter(element => element.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre === maxSugar[0].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre)
+        
+        console.log("Donut con más proteína:");
+        
+        maxFibre.map(element => {
+            console.log(element.name);
+        })
+
+        //Listar todos los donuts y sus carbohidratos
+        //Mostrar la media de calorías de entre todos los donuts
+        //Mostrar la suma de las grasas saturadas de todos los donuts
+        //Mostrar el porcentaje medio de cada vitamina
 
     } catch (error){
         console.log(error.menssage)
